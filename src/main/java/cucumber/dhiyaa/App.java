800d3cfb7b4cf10469842bba8aad3b9bb0605d54
Training_C2A.04.30 <Training_C2A.04.30@CDC2-D-CJ4SXJ2.dir.svc.accenture.com>
2019-07-10 13:52:51 +0530

    committed successfully

---
+++ src/main/java/cucumber/dhiyaa/App.java
@@ -0,0 +1,13 @@
+package cucumber.dhiyaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/vbvb/NewTest.java
@@ -0,0 +1,9 @@
+package vbvb;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
